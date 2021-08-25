d9a9a48e66c2394849b57022a81c65694c2d6940
zy <zy@747.com>
2021-08-25 12:49:45 +0800

    Eureka-configCenter

---
+++ cloud-config-center-3344/src/main/java/com/zy/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.zy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author zy
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+
+}
