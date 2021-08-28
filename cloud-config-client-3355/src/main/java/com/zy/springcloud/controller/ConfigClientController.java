75ef1e95574f324f21bf8139919d5d5012debd86
zy <zy@747.com>
2021-08-28 09:57:00 +0800

    Eureka-Ribbon-openFeign-Hystrix-GateWay-ConfigCenter

---
+++ cloud-config-client-3355/src/main/java/com/zy/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.zy.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zy
+ */
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo + " serverPort: " + serverPort;
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/zy/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.zy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zy
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
+
